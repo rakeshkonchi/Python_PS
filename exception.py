4bdb49f9c463b6ed9e02606f358c8de0a97caf9c
rakeshkonchi <rakeshkonchi2002@gamil.com>
2026-06-05 21:57:52 +0530

    Modified Exception file

--- exception.py
+++ exception.py
@@ -12,3 +12,41 @@ try:
 except Exception as e:
     print("Hello")
     print(e)
+
+def fun1():
+    print("Entering fun1")
+    res=10/0
+    print(res)
+    print("leaving fun1")
+def fun2():
+    print("Entering fun2")
+    try:
+        fun1()
+    except Exception as e:
+        print("Error")
+    print("leaving fun2")
+print("Program started")
+fun2()
+print("Program ended")
+
+A=int(input("Enter a number"))
+B=int(input("Enter another number"))
+try:
+    C=A/B
+    print(C)
+except Exception as e:
+    print("Error")
+else:
+    print("No error")
+
+A=int(input("Enter a number"))
+B=int(input("Enter another number"))
+try:
+    C=A/B
+    print(C)
+except Exception as e:
+    print("Error")
+else:
+    print("Error")
+finally:
+    print("Program execution completed")
